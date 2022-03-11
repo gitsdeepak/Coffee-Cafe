cc797fbfcbcf13560fd77e20d28e05bc35468812
kimnamjoon123 <poojasaini152018@gmail.com>
2022-03-11 17:51:12 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,44 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+ document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+const dark = document.getElementById("dark");
+const theme = document.querySelector("#theme-link");
+dark.addEventListener("click", function() {
+  if (theme.getAttribute("href") == "style.css") {
+    theme.href = "stylesheet.css";
+  } else {
+    theme.href = "style.css";
+  }
+});
+
+
+
+
+
